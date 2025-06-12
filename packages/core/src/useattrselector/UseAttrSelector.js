cf37a2cc301e33168a1a9c0098a11bd52dc611fe
peacepiece7 <peacepiece7@naver.com>
2025-06-12 17:45:40 +0900

    refactor: update 'pc' for default parameter to 'data-pc'

--- packages/core/src/useattrselector/UseAttrSelector.js
+++ packages/core/src/useattrselector/UseAttrSelector.js
@@ -1,6 +1,6 @@
 import { useId } from 'vue';
 
-export function useAttrSelector(prefix = 'pc') {
+export function useAttrSelector(prefix = 'data-pc') {
     const idx = useId();
 
     return `${prefix}${idx.replace('v-', '').replaceAll('-', '_')}`;
